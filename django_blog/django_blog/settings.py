b62c6a7a9ba0da762fa7309212cd5f081cdf5f9a
LeonePhisher <thackme39@gmail.com>
2025-12-03 02:20:22 +0000

    Change database engine to PostgreSQL
    Updated database settings to use PostgreSQL.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -92,9 +92,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_blog",
+        "USER": "myuser",         # <-- required
+        "PASSWORD": "mypassword", # <-- required
+        "HOST": "localhost",
+        "PORT": "5432",           # <-- required
     }
 }
 
@@ -138,3 +142,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
