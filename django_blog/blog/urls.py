438556a7881b860345006c72e9c3d41ca325b7fb
LeonePhisher <thackme39@gmail.com>
2025-12-03 02:25:12 +0000

    Refactor URL patterns and import statements
    Refactored import statements for views and organized code structure.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 app_name = 'blog'
 
@@ -25,3 +31,4 @@ urlpatterns = [
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
+
