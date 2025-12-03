f306c237ecf842c065b499896bf0338cbdb54005
LeonePhisher <thackme39@gmail.com>
2025-12-03 02:48:46 +0000

    Add 'taggit' to installed apps in settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'taggit',
     
 
 ]
@@ -142,4 +143,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
