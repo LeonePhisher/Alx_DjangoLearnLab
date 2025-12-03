59b55dc448aa937e77eb7ed963115255e56b5473
LeonePhisher <thackme39@gmail.com>
2025-12-03 02:38:32 +0000

    Add comment CRUD paths to blog URLs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,27 +1,24 @@
-from django.urls import path
 from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
 )
 
 urlpatterns = [
-    # List all posts
+    # Post CRUD
     path('', PostListView.as_view(), name='post_list'),
-     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
-
-    # View single post
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-
-    # Create a new post
     path('post/new/', PostCreateView.as_view(), name='post_create'),
-
-    # Update an existing post
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
-
-    # Delete an existing post
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-]
 
+    # Comment CRUD
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+]
